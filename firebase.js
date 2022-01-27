ca66f65b5577d9b3a3ec8ab7843c366cfe53e6cc
Revem <revemvi@hotmail.com>
2022-01-27 16:02:27 -0300

    adicionado firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9jeUfu7d1ecD5j7tfuCFAyZBZd9HqIpc",
+  authDomain: "vibot-e9365.firebaseapp.com",
+  projectId: "vibot-e9365",
+  storageBucket: "vibot-e9365.appspot.com",
+  messagingSenderId: "493112956684",
+  appId: "1:493112956684:web:2369b89760a7bea0321b29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
